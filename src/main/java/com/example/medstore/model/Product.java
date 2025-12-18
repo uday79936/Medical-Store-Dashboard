90fab416fd4712dc2e83a954a36400223c23ca88
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2025-12-18 12:34:59 +0530

    Update Product.java

--- src/main/java/com/example/medstore/model/Product.java
+++ src/main/java/com/example/medstore/model/Product.java
@@ -1,74 +1,19 @@
-package com.example.medstore.model;
+package com.example.medstore.service;
 
-import java.util.Objects;
+import com.example.medstore.model.Product;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Product {
+public class ProductService {
 
-    private String code;
-    private String name;
-    private int stock;
-    private double price;
+    public List<Product> getAllProducts() {
 
-    public Product() {}
+        List<Product> products = new ArrayList<>();
 
-    public Product(String code, String name, int stock, double price) {
-        this.code = code;
-        this.name = name;
-        this.stock = stock;
-        this.price = price;
-    }
+        products.add(new Product("P001", "Paracetamol", 50, 2.50));
+        products.add(new Product("P002", "Vitamin C", 200, 1.80));
+        products.add(new Product("P003", "Ibuprofen", 100, 3.20));
 
-    // Getters
-    public String getCode() { return code; }
-    public String getName() { return name; }
-    public int getStock() { return stock; }
-    public double getPrice() { return price; }
-
-    // Setters
-    public void setCode(String code) { this.code = code; }
-    public void setName(String name) { this.name = name; }
-
-    public void setStock(int stock) {
-        if (stock < 0) {
-            throw new IllegalArgumentException("Stock cannot be negative");
-        }
-        this.stock = stock;
-    }
-
-    public void setPrice(double price) {
-        if (price < 0) {
-            throw new IllegalArgumentException("Price must be positive");
-        }
-        this.price = price;
-    }
-
-    // Business helper methods
-    public boolean isInStock() {
-        return this.stock > 0;
-    }
-
-    // equals & hashCode (based on code)
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Product)) return false;
-        Product product = (Product) o;
-        return Objects.equals(code, product.code);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(code);
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "Product{" +
-                "code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", stock=" + stock +
-                ", price=" + price +
-                '}';
+        return products;
     }
 }
