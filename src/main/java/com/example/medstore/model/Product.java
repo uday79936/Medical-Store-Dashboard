6c2a56262d11689aa33af4fd7f7de836bc1e6fe4
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2025-12-18 12:44:59 +0530

    Update Product.java

--- src/main/java/com/example/medstore/model/Product.java
+++ src/main/java/com/example/medstore/model/Product.java
@@ -16,35 +16,15 @@ public class Product {
         this.price = price;
     }
 
-    public String getCode() {
-        return code;
-    }
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 }
