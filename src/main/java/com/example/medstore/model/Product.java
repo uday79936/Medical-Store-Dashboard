214d38e9ee41ece7713ef93853bde31de94be7ce
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2026-07-12 17:55:16 +0530

    Update Product.java

--- src/main/java/com/example/medstore/model/Product.java
+++ src/main/java/com/example/medstore/model/Product.java
@@ -1,5 +1,7 @@
 package com.example.medstore.model;
 
+import java.util.Objects;
+
 public class Product {
 
     private String code;
@@ -7,24 +9,82 @@ public class Product {
     private int stock;
     private double price;
 
-    public Product(){}
-
-    public Product(String code,String name,int stock,double price){
-        this.code=code;
-        this.name=name;
-        this.stock=stock;
-        this.price=price;
+    // Default Constructor
+    public Product() {
     }
 
-    public String getCode(){ return code; }
-    public void setCode(String code){ this.code=code; }
+    // Parameterized Constructor
+    public Product(String code, String name, int stock, double price) {
+        this.code = code;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    }
 
-    public String getName(){ return name; }
-    public void setName(String name){ this.name=name; }
+    // Getters and Setters
+    public String getCode() {
+        return code;
+    }
 
-    public int getStock(){ return stock; }
-    public void setStock(int stock){ this.stock=stock; }
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-    public double getPrice(){ return price; }
-    public void setPrice(double price){ this.price=price; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        if (stock >= 0) {
+            this.stock = stock;
+        } else {
+            throw new IllegalArgumentException("Stock cannot be negative.");
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            throw new IllegalArgumentException("Price cannot be negative.");
+        }
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Product{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", stock=" + stock +
+                ", price=" + price +
+                '}';
+    }
+
+    // equals Method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(code, product.code);
+    }
+
+    // hashCode Method
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
 }
